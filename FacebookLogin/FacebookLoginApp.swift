0b37638443cb9f0c5dcbba1628e7e7975c3805cd
giovannamoeller <giovannagmoeller@gmail.com>
2021-11-21 21:57:34 -0300

    Finishing Project

---
+++ FacebookLogin/FacebookLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookLoginApp.swift
+//  FacebookLogin
+//
+//  Created by Giovanna Moeller on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FacebookLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
